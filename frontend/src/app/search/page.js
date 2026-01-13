6475f5bc4c7775f191d218048398eba1117e45ad
aplus <beinghuman5320@gmail.com>
2026-01-14 01:36:30 +0600

    gergrefg

--- frontend/src/app/search/page.js
+++ frontend/src/app/search/page.js
@@ -1,15 +1,13 @@
 import Searchresult from "./Searchresult";
 
 export default function Page({ searchParams }) {
-
-  const q = searchParams.q || "";
-  const mainCategory = searchParams.mainCategory || "";
-  const subCategory = searchParams.subCategory || "";
-
-
   return (
     <>
-      <Searchresult key={`${q}-${mainCategory}-${subCategory}`} />
+      <Searchresult
+      q={searchParams.q || ""}
+      mainCategory={searchParams.mainCategory || ""}
+      subCategory={searchParams.subCategory || ""}
+    />
     </>
   );
 }
