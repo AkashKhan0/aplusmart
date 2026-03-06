50d5fb4fdea0a9142e73b449dc03142d18212ae2
aplus <beinghuman5320@gmail.com>
2026-03-07 00:06:03 +0600

    some changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,23 +3,25 @@ import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import helmet from "helmet";
 
 dotenv.config();
 const app = express();
-app.use(express.json());
 app.use(
   cors({
     origin: [
       "http://localhost:3000",
       "http://localhost:3001",
       "https://aplusmartfrontend.vercel.app",
-      "https://aplusmartadmin.vercel.app"
+      "https://aplusmartadmin.vercel.app",
+      "https://aplusmartbd.com"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true, 
   })
 );
 
+app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
 
